d5d73c8488863a043717e629ee506f59342376d4
csebineza-rrc <csebineza@academic.rrc.ca>
2026-01-16 10:52:43 -0600

    Add header and footer components with styling

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+function Footer() {
+    return (
+    <footer>
+        Built By Pavel Pirogov, Ian Lamb & Cedrick Sebineza
+    </footer>
+    )
+}
+
+export default Footer
