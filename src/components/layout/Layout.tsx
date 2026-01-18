f5f212cf527aeb04bb33cc5a729487b1d5835a86
csebineza-rrc <csebineza@academic.rrc.ca>
2026-01-18 15:21:04 -0600

    Fix Layout component to use correct imports and add children prop

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,22 @@
-import { Footer } from "./footer/footer";
-import { Nav } from "./nav/nav";
-import { Header } from "./header/header";
+import Footer from "../landing/footer/footer";
+import Nav from "../landing/nav/nav";
+import Header from "../landing/header/header";
+import type { ReactNode } from "react";
 
-export function Layout() {
-    return(
+interface Props {
+    title: string;
+    children: ReactNode;
+};
+
+export function Layout({children}: Props) {
+    return (
         <>
-         <Nav />
-         <Header title="vite-project" />
-
-        <main>
-            {children}
-        </main>
-
-        <Footer />
-        <>
+            <Nav />
+            <Header title="vite-project" />
+            <main>
+                {children}
+            </main>
+            <Footer />
+        </>
     );
 }
