ac304337a94e73ca624d0832a45b632d18a09931
ppirogov-rrc <ppirogov@rrc.ca>
2026-01-18 16:59:19 -0600

    Fixed the expenses import issue.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Dashboard from './components/dashboard/dashboard';
 import Income from './components/income/income';
-import Expenses from './components/expense/expense'
+import { Expenses } from './components/expense/expense'
 import { Layout } from './components/layout/Layout';
 
 function App() {
