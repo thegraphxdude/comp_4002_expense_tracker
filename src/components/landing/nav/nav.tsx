bc4dfbf24cc2484d8b848a24f3a89b14235647a4
ppirogov-rrc <ppirogov@rrc.ca>
2026-02-01 12:53:02 -0600

    Updated the navigation to using Links instead of a tags.

--- src/components/landing/nav/nav.tsx
+++ src/components/landing/nav/nav.tsx
@@ -1,12 +1,13 @@
 import "./nav.css"
+import { Link } from "react-router-dom";
 
 function Nav() {
     return (
         <nav>
             <div id="navigation">
-                <a href="/">Dashboard</a>
-                <a href="/expenses">Expenses</a>
-                <a href="/income">income</a>
+                <Link to="/">Dashboard</Link>
+                <Link to="/expenses">Expenses</Link>
+                <Link to="/income">Income</Link>
             </div>
         </nav>
     );
